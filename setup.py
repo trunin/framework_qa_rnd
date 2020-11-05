4b753ff4e85b043d78d7ffbc208c5b5de82df00e
e.trunin <e.trunin@corp.mail.ru>
2020-11-05 17:49:49 +0300

    adqwcq

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='QA_framework',
-    version="0.0.1",
+    version="0.0.2",
     author='e.trunin',
     author_email='e.trunin@corp.mail.ru',
     description="qa framework for rnd team",
